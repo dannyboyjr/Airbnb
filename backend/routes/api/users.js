f68f2aa0779f2902a8639e8decfe9194c18b254b
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2022-11-09 18:58:32 -0700

    creates user or throws Error 403 if email already exist

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -8,47 +8,44 @@ const { handleValidationErrors } = require('../../utils/validation')
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-      const { email, password, username } = req.body;
-      const user = await User.signup({ email, username, password });
-  
-      await setTokenCookie(res, user);
-  
-      return res.json({user});
+
+//validateSignup that will check keys and validate them:
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
+router.post('/', validateSignup, async (req, res) => {
+  const { firstName, lastName, email, password, username } = req.body;
+  try {
+    const user = await User.signup({ firstName, lastName, email, username, password });
+
+    let token = await setTokenCookie(res, user);
+    user.dataValues.token = token;
+    return res.json({
+      user,
     });
-
-    //validateSignup that will check keys and validate them:
-    const validateSignup = [
-      check('email')
-        .exists({ checkFalsy: true })
-        .isEmail()
-        .withMessage('Please provide a valid email.'),
-      check('username')
-        .exists({ checkFalsy: true })
-        .isLength({ min: 4 })
-        .withMessage('Please provide a username with at least 4 characters.'),
-      check('username')
-        .not()
-        .isEmail()
-        .withMessage('Username cannot be an email.'),
-      check('password')
-        .exists({ checkFalsy: true })
-        .isLength({ min: 6 })
-        .withMessage('Password must be 6 characters or more.'),
-      handleValidationErrors
-    ];
-
-    router.post('/', validateSignup, async (req, res) => {
-        const { email, password, username } = req.body;
-        const user = await User.signup({ email, username, password });
-    
-        await setTokenCookie(res, user);
-    
-        return res.json({
-          user,
-        });
-      }
-    );
+  } catch (e) {
+    res.status(403)
+    return res.json({ message: "Email already Exists" })
+  }
+}
+);
 
 
 
