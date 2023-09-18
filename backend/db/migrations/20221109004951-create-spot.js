d07c8b84372c47eecceb3ad631c725418a93f3c4
Daniel Kimball <danieljameskimball@gmail.com>
2023-09-18 10:57:44 -0600

    changed description from STRING to TEXT

--- backend/db/migrations/20221109004951-create-spot.js
+++ backend/db/migrations/20221109004951-create-spot.js
@@ -44,7 +44,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       price: {
         type: Sequelize.DECIMAL,
